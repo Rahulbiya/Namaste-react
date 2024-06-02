0f2277b491e7ee17654e8d7c260676d4c632f56d
Rahulbiya <87463848+Rahulbiya@users.noreply.github.com>
2024-06-02 16:52:45 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,9 @@
+const heading =React.createElement('h1',{id:'heading',xyz:"abc"},'Hello World');
+//const heading is a react elemnet is normal object in java script.
+//heading and xyz are props of h1 element.
+const root= ReactDOM.createRoot(document.getElementById('root'));
+root.render(heading);
+
+const parent = React.createElement('div',{id:"parent"},React.createElement('div',{id:'child'},React.createElement('h1',{},'I asm H1 tag')));
+console.log(parent);
+root.render(parent);
