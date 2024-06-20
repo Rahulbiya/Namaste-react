d651450d8f849aa41928ee7f98af06a2943f296c
Rahulbiya <87463848+Rahulbiya@users.noreply.github.com>
2024-06-20 09:32:08 +0530

    ..

--- App.js
+++ App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-const heading =React.createElement('h1',{id:'heading',xyz:"abc"},'Hello World');
-//const heading is a react elemnet is normal object in java script.
-//heading and xyz are props of h1 element.
-const root= ReactDOM.createRoot(document.getElementById('root'));
+import ReactDOM from 'react-dom/client';
+const heading =React.createElement("h1",{id:"heading"},"Hello world");
+console.log(heading);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(heading);
-
-const parent = React.createElement('div',{id:"parent"},React.createElement('div',{id:'child'},React.createElement('h1',{},'I asm H1 tag')));
-console.log(parent);
-root.render(parent);
